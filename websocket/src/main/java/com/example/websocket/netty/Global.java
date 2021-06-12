8c47bf89819086eb573f89caa2f89286128e6b77
xiebing <18671989765@163.com>
2021-06-12 11:59:22 +0800

    test

--- websocket/src/main/java/com/example/websocket/netty/Global.java
+++ websocket/src/main/java/com/example/websocket/netty/Global.java
@@ -3,7 +3,11 @@ package com.example.websocket.netty;
 import io.netty.channel.group.ChannelGroup;
 import io.netty.channel.group.DefaultChannelGroup;
 import io.netty.util.concurrent.GlobalEventExecutor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
+@Configuration
+@Import({ChildChannelHandler.class})
 public class Global {
 
     /**
